5b2ec28cb072d8457fa71130b0280bc56ea635a0
Julius L. Masigan <juliusmasigan@gmail.com>
2016-02-17 20:39:34 +0800

    First Commit.

---
+++ schema.sql
@@ -0,0 +1,313 @@
+-- MySQL dump 10.13  Distrib 5.6.28, for debian-linux-gnu (i686)
+--
+-- Host: localhost    Database: zemsoftcrm
+-- ------------------------------------------------------
+-- Server version	5.6.28-0ubuntu0.15.10.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `adssite`
+--
+
+DROP TABLE IF EXISTS `adssite`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `adssite` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `adssiteno` varchar(30) DEFAULT NULL,
+  `sitename` varchar(30) DEFAULT NULL,
+  `city` varchar(30) DEFAULT NULL,
+  `location` varchar(100) DEFAULT NULL,
+  `numberoffaces` varchar(30) DEFAULT NULL,
+  `owner` varchar(30) DEFAULT NULL,
+  `createduser` varchar(30) DEFAULT NULL,
+  `createddatetime` datetime DEFAULT NULL,
+  `lastupdateduser` varchar(30) DEFAULT NULL,
+  `lastupdateddatetime` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `adssite`
+--
+
+LOCK TABLES `adssite` WRITE;
+/*!40000 ALTER TABLE `adssite` DISABLE KEYS */;
+/*!40000 ALTER TABLE `adssite` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `department`
+--
+
+DROP TABLE IF EXISTS `department`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `department` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `departmentname` varchar(30) DEFAULT NULL,
+  `active` bit(1) DEFAULT NULL,
+  `createduser` varchar(30) DEFAULT NULL,
+  `createddatetime` datetime DEFAULT NULL,
+  `updateduser` varchar(30) DEFAULT NULL,
+  `updateddatetime` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `department`
+--
+
+LOCK TABLES `department` WRITE;
+/*!40000 ALTER TABLE `department` DISABLE KEYS */;
+/*!40000 ALTER TABLE `department` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `numberseries`
+--
+
+DROP TABLE IF EXISTS `numberseries`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `numberseries` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `objectid` int(11) DEFAULT NULL,
+  `prefix` varchar(5) DEFAULT NULL,
+  `startingno` varchar(30) DEFAULT NULL,
+  `endingno` varchar(30) DEFAULT NULL,
+  `increment` int(11) DEFAULT NULL,
+  `lastnumberused` varchar(30) DEFAULT NULL,
+  `active` bit(1) DEFAULT NULL,
+  `createduser` varchar(30) DEFAULT NULL,
+  `createddatetime` datetime DEFAULT NULL,
+  `updateduser` varchar(30) DEFAULT NULL,
+  `updateddatetime` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `numberseries`
+--
+
+LOCK TABLES `numberseries` WRITE;
+/*!40000 ALTER TABLE `numberseries` DISABLE KEYS */;
+/*!40000 ALTER TABLE `numberseries` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `permission`
+--
+
+DROP TABLE IF EXISTS `permission`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `permission` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `roleid` int(11) DEFAULT NULL,
+  `objectid` int(11) DEFAULT NULL,
+  `view` tinyint(1) DEFAULT NULL,
+  `create` tinyint(1) DEFAULT NULL,
+  `edit` tinyint(1) DEFAULT NULL,
+  `delete` tinyint(1) DEFAULT NULL,
+  `prepare` tinyint(1) DEFAULT NULL,
+  `check` tinyint(1) DEFAULT NULL,
+  `approve` tinyint(1) DEFAULT NULL,
+  `cancel` tinyint(1) DEFAULT NULL,
+  `active` bit(1) DEFAULT NULL,
+  `tsstamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `permission`
+--
+
+LOCK TABLES `permission` WRITE;
+/*!40000 ALTER TABLE `permission` DISABLE KEYS */;
+/*!40000 ALTER TABLE `permission` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `position`
+--
+
+DROP TABLE IF EXISTS `position`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `position` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(30) DEFAULT NULL,
+  `active` bit(1) DEFAULT NULL,
+  `tsstamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `position`
+--
+
+LOCK TABLES `position` WRITE;
+/*!40000 ALTER TABLE `position` DISABLE KEYS */;
+/*!40000 ALTER TABLE `position` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `profile`
+--
+
+DROP TABLE IF EXISTS `profile`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `profile` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `honorific` varchar(25) DEFAULT NULL,
+  `firstname` varchar(100) DEFAULT NULL,
+  `middlename` varchar(100) DEFAULT NULL,
+  `lastname` varchar(100) DEFAULT NULL,
+  `gender` varchar(30) DEFAULT NULL,
+  `address` varchar(100) DEFAULT NULL,
+  `city` varchar(30) DEFAULT NULL,
+  `contactno` varchar(30) DEFAULT NULL,
+  `emailaddress` varchar(60) DEFAULT NULL,
+  `departmentid` int(11) DEFAULT NULL,
+  `positionid` int(11) DEFAULT NULL,
+  `photo` varchar(255) DEFAULT NULL,
+  `createduser` varchar(30) DEFAULT NULL,
+  `createddatetime` datetime DEFAULT NULL,
+  `updateduser` varchar(30) DEFAULT NULL,
+  `updateddatetime` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `profile`
+--
+
+LOCK TABLES `profile` WRITE;
+/*!40000 ALTER TABLE `profile` DISABLE KEYS */;
+/*!40000 ALTER TABLE `profile` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `role`
+--
+
+DROP TABLE IF EXISTS `role`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `role` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `rolename` varchar(45) DEFAULT NULL,
+  `active` bit(1) DEFAULT NULL,
+  `tsstamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `role`
+--
+
+LOCK TABLES `role` WRITE;
+/*!40000 ALTER TABLE `role` DISABLE KEYS */;
+/*!40000 ALTER TABLE `role` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `siteface`
+--
+
+DROP TABLE IF EXISTS `siteface`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `siteface` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `sitefaceno` varchar(30) DEFAULT NULL,
+  `siteadsid` varchar(30) DEFAULT NULL,
+  `description` varchar(255) DEFAULT NULL,
+  `sitefacetype` varchar(30) DEFAULT NULL,
+  `sitefaceclassification` varchar(30) DEFAULT NULL,
+  `trafficbound` varchar(30) DEFAULT NULL,
+  `structuretype` varchar(30) DEFAULT NULL,
+  `dimension` varchar(30) DEFAULT NULL,
+  `materials` varchar(30) DEFAULT NULL,
+  `lights` varchar(30) DEFAULT NULL,
+  `sitephoto` varchar(255) DEFAULT NULL,
+  `maplocation` varchar(255) DEFAULT NULL,
+  `remarks` varchar(255) DEFAULT NULL,
+  `active` bit(1) DEFAULT NULL,
+  `createduser` varchar(30) DEFAULT NULL,
+  `createddatetime` datetime DEFAULT NULL,
+  `lastupdateduser` varchar(30) DEFAULT NULL,
+  `lastupdateddatetime` datetime DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `siteface`
+--
+
+LOCK TABLES `siteface` WRITE;
+/*!40000 ALTER TABLE `siteface` DISABLE KEYS */;
+/*!40000 ALTER TABLE `siteface` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(225) NOT NULL,
+  `password` varchar(32) NOT NULL,
+  `active` bit(1) NOT NULL DEFAULT b'0',
+  `profileid` int(11) DEFAULT NULL,
+  `roles` text,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username_UNIQUE` (`username`),
+  KEY `profileid_FOREIGN` (`profileid`),
+  CONSTRAINT `fk_profileid_profile` FOREIGN KEY (`id`) REFERENCES `profile` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-02-17 20:37:14
