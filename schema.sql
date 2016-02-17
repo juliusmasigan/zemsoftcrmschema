cc909f95b569a570db83aa8479ccc7435a9abac6
Julius L. Masigan <juliusmasigan@gmail.com>
2016-02-17 22:57:09 +0800

    Schema Updates.

--- schema.sql
+++ schema.sql
@@ -56,12 +56,9 @@ DROP TABLE IF EXISTS `department`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `department` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `departmentname` varchar(30) DEFAULT NULL,
-  `active` bit(1) DEFAULT NULL,
-  `createduser` varchar(30) DEFAULT NULL,
-  `createddatetime` datetime DEFAULT NULL,
-  `updateduser` varchar(30) DEFAULT NULL,
-  `updateddatetime` datetime DEFAULT NULL,
+  `name` varchar(255) NOT NULL,
+  `active` bit(1) NOT NULL DEFAULT b'0',
+  `timestamp` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -109,6 +106,32 @@ LOCK TABLES `numberseries` WRITE;
 UNLOCK TABLES;
 
 --
+-- Table structure for table `object`
+--
+
+DROP TABLE IF EXISTS `object`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `object` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `module` varchar(255) NOT NULL,
+  `active` bit(1) NOT NULL DEFAULT b'0',
+  `timestamp` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `object`
+--
+
+LOCK TABLES `object` WRITE;
+/*!40000 ALTER TABLE `object` DISABLE KEYS */;
+/*!40000 ALTER TABLE `object` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `permission`
 --
 
@@ -117,19 +140,23 @@ DROP TABLE IF EXISTS `permission`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `permission` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `roleid` int(11) DEFAULT NULL,
-  `objectid` int(11) DEFAULT NULL,
-  `view` tinyint(1) DEFAULT NULL,
-  `create` tinyint(1) DEFAULT NULL,
-  `edit` tinyint(1) DEFAULT NULL,
-  `delete` tinyint(1) DEFAULT NULL,
-  `prepare` tinyint(1) DEFAULT NULL,
-  `check` tinyint(1) DEFAULT NULL,
-  `approve` tinyint(1) DEFAULT NULL,
-  `cancel` tinyint(1) DEFAULT NULL,
-  `active` bit(1) DEFAULT NULL,
+  `roleid` int(11) NOT NULL,
+  `objectid` int(11) NOT NULL,
+  `view` tinyint(1) NOT NULL DEFAULT '0',
+  `create` tinyint(1) NOT NULL DEFAULT '0',
+  `edit` tinyint(1) NOT NULL DEFAULT '0',
+  `delete` tinyint(1) NOT NULL DEFAULT '0',
+  `prepare` tinyint(1) NOT NULL DEFAULT '0',
+  `check` tinyint(1) NOT NULL DEFAULT '0',
+  `approve` tinyint(1) NOT NULL DEFAULT '0',
+  `cancel` tinyint(1) NOT NULL DEFAULT '0',
+  `active` bit(1) NOT NULL DEFAULT b'0',
   `tsstamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `roleid_FOREIGN` (`roleid`),
+  KEY `objectid_FOREIGN` (`objectid`),
+  CONSTRAINT `fk_permission_objectid` FOREIGN KEY (`objectid`) REFERENCES `object` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_permission_roleid` FOREIGN KEY (`roleid`) REFERENCES `role` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -151,8 +178,8 @@ DROP TABLE IF EXISTS `position`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `position` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(30) DEFAULT NULL,
-  `active` bit(1) DEFAULT NULL,
+  `name` varchar(255) NOT NULL,
+  `active` bit(1) NOT NULL DEFAULT b'0',
   `tsstamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -177,22 +204,23 @@ DROP TABLE IF EXISTS `profile`;
 CREATE TABLE `profile` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `honorific` varchar(25) DEFAULT NULL,
-  `firstname` varchar(100) DEFAULT NULL,
-  `middlename` varchar(100) DEFAULT NULL,
-  `lastname` varchar(100) DEFAULT NULL,
+  `firstname` varchar(255) DEFAULT NULL,
+  `middlename` varchar(255) DEFAULT NULL,
+  `lastname` varchar(255) DEFAULT NULL,
   `gender` varchar(30) DEFAULT NULL,
-  `address` varchar(100) DEFAULT NULL,
-  `city` varchar(30) DEFAULT NULL,
+  `address` varchar(500) DEFAULT NULL,
+  `city` varchar(255) DEFAULT NULL,
   `contactno` varchar(30) DEFAULT NULL,
-  `emailaddress` varchar(60) DEFAULT NULL,
+  `emailaddress` varchar(162) DEFAULT NULL,
   `departmentid` int(11) DEFAULT NULL,
   `positionid` int(11) DEFAULT NULL,
   `photo` varchar(255) DEFAULT NULL,
-  `createduser` varchar(30) DEFAULT NULL,
-  `createddatetime` datetime DEFAULT NULL,
-  `updateduser` varchar(30) DEFAULT NULL,
-  `updateddatetime` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  `timestamp` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `positionid_FOREIGN` (`positionid`),
+  KEY `departmentid_FOREIGN` (`departmentid`),
+  CONSTRAINT `fk_profile_departmentid` FOREIGN KEY (`departmentid`) REFERENCES `department` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_profile_positionid` FOREIGN KEY (`positionid`) REFERENCES `position` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -214,8 +242,8 @@ DROP TABLE IF EXISTS `role`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `role` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `rolename` varchar(45) DEFAULT NULL,
-  `active` bit(1) DEFAULT NULL,
+  `name` varchar(255) NOT NULL,
+  `active` bit(1) NOT NULL DEFAULT b'0',
   `tsstamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -279,16 +307,15 @@ DROP TABLE IF EXISTS `user`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(225) NOT NULL,
+  `username` varchar(255) NOT NULL,
   `password` varchar(32) NOT NULL,
   `active` bit(1) NOT NULL DEFAULT b'0',
   `profileid` int(11) DEFAULT NULL,
-  `roles` text,
   `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `username_UNIQUE` (`username`),
+  UNIQUE KEY `username_UNIQUE` (`username`,`active`),
   KEY `profileid_FOREIGN` (`profileid`),
-  CONSTRAINT `fk_profileid_profile` FOREIGN KEY (`id`) REFERENCES `profile` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  CONSTRAINT `fk_user_profileid` FOREIGN KEY (`id`) REFERENCES `profile` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -300,6 +327,35 @@ LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `userrole`
+--
+
+DROP TABLE IF EXISTS `userrole`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `userrole` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `userid` int(11) NOT NULL,
+  `roleid` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `userid_UNIQUE` (`userid`,`roleid`),
+  KEY `userid_FOREIGN` (`userid`),
+  KEY `roleid_FOREIGN` (`roleid`),
+  CONSTRAINT `fk_userrole_roleid` FOREIGN KEY (`roleid`) REFERENCES `role` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_userrole_userid` FOREIGN KEY (`userid`) REFERENCES `user` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `userrole`
+--
+
+LOCK TABLES `userrole` WRITE;
+/*!40000 ALTER TABLE `userrole` DISABLE KEYS */;
+/*!40000 ALTER TABLE `userrole` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -310,4 +366,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-02-17 20:37:14
+-- Dump completed on 2016-02-17 22:46:06
